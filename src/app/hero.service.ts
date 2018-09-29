8c79eaaa89c3a5fd5cac1e76c6f15cf5893e4c97
MAshraf <ee.mashraf@gmail.com>
2018-09-29 17:14:44 +0200

    message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './seed-hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched Heroes');
     return of(HEROES);
   }
 }
