dc0ba0050418690a33cc9113e84587361c161562
MAshraf <ee.mashraf@gmail.com>
2018-09-29 22:12:02 +0200

    more navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,10 +15,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
